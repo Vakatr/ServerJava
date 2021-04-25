c3e86e25e8ef093050cc168d682171138e189e81
ЯкушевскийВВ <40803132+Vakatr@users.noreply.github.com>
2021-04-25 16:54:27 +0300

    Update WebSecurityConfig.java

--- src/main/java/com/legist/myapp/config/WebSecurityConfig.java
+++ src/main/java/com/legist/myapp/config/WebSecurityConfig.java
@@ -10,9 +10,9 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
 @EnableWebSecurity
@@ -21,7 +21,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter implements W
     private final JwtTokenProvider jwtTokenProvider;
 
     private static final String ADMIN_ENDPOINT = "/api/v1/admin/**";
-    private static final String MODERATOR_ENDPOINT = "/api/v1/admin/**";
+    private static final String LEGIST_ENDPOINT = "/api/v1/legist/**";
     private static final String LOGIN_ENDPOINT = "/api/v1/auth/login";
     private static final String REGISTER_ENDPOINT = "/api/v1/auth/register";
     private static final String GUEST_ENDPOINT = "/api/v1/message/";
@@ -41,25 +41,25 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter implements W
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .httpBasic().disable()
-                .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authorizeRequests()
                 .antMatchers(LOGIN_ENDPOINT).permitAll()
                 .antMatchers(REGISTER_ENDPOINT).permitAll()
                 .antMatchers(GUEST_ENDPOINT).permitAll()
-             //   .antMatchers(ADMIN_ENDPOINT).hasAuthority("ADMIN")
-             //   .antMatchers(MODERATOR_ENDPOINT).hasAuthority("MODERATOR")
+                .antMatchers(ADMIN_ENDPOINT).hasAuthority("ADMIN")
+                .antMatchers(LEGIST_ENDPOINT).hasAuthority("LEGIST")
                 .anyRequest().authenticated()
                 .and()
-                .apply(new JwtConfigurer(jwtTokenProvider));
+                .apply(new JwtConfigurer(jwtTokenProvider))
+                .and()
+               .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
     }
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
-				.allowedHeaders("*")
+                .allowedOrigins("https://testserverv.herokuapp.com")
                 .allowedMethods("*");
     }
 }
